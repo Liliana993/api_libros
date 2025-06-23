3806bc226065b1bf5611aedb0098034405db3293
Liliana993 <lili_esco@yahoo.com.ar>
2025-06-23 11:13:26 -0300

    modificaciones en la carpeta

--- app.js
+++ app.js
@@ -4,24 +4,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const dbConnect = require('./config/db');
-const librosRoutes = require('./routes/libro');
-
-//Middlewares
-const loggingMiddleware = require('./middleware/loggingMiddleware');
-const errorMiddleware = require('./middleware/errorMiddleware');
-const notFoundMiddleware = require('./middleware/notFoundMiddleware');
+const todoRoutes = require('./routes/todoRouter');
 
 //Cors
 const cors = require('cors');
 app.use(cors());
 
-app.use(loggingMiddleware);//Usamos el middleware del logging en toda la app
-
 app.use(express.json());
-app.use("/api",librosRoutes);
-
-app.use(notFoundMiddleware); //Usamos el middleware de notfound para ruta no encontrada
-app.use(errorMiddleware);//usamos el middleware de error en toda la app
+app.use("/api",todoRoutes);
 
 
 //conectar a la base de datos
--- middleware/notFoundMiddleware.js
+++
@@ -1,9 +0,0 @@
-// middlewares/notFoundMiddleware.js
-
-const notFoundMiddleware = (req, res, next) => {
-
-    res.status(404).send({ mensaje: 'Ruta no encontrada' });
-
-};
-
-module.exports = notFoundMiddleware;
